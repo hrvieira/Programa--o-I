e326fa86870c40faa24108ce348ef0ce47ec4ef0
hrvieira <luizhrvieira@outlook.com>
2024-08-22 20:03:16 -0300

    first commit

---
+++ operadores1.java
@@ -0,0 +1,19 @@
+/**
+ * operadores1
+ */
+public class operadores1 {
+
+     public static void main(String[] args) {
+          
+
+          double nota1 = 9.0;
+          double nota2 = 10;
+
+          double media;
+
+          media = (nota1 + nota2)/2;
+
+          System.out.println(media);
+
+     }
+}
